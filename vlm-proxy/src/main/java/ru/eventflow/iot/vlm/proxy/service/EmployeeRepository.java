c87bdf9fbc38110e72fc18f52fae6c41721a889d
Konstantin Sokolov <vtqveant@gmail.com>
2024-09-29 22:09:28 +0300

    logback config; transaction trace in log; connection pool trace in log

--- vlm-proxy/src/main/java/ru/eventflow/iot/vlm/proxy/service/EmployeeRepository.java
+++ vlm-proxy/src/main/java/ru/eventflow/iot/vlm/proxy/service/EmployeeRepository.java
@@ -1,11 +1,18 @@
 package ru.eventflow.iot.vlm.proxy.service;
 
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.eventflow.iot.vlm.proxy.domain.Employee;
 
 public interface EmployeeRepository {
 
+    /**
+     * set readOnly=true for retrieval-only methods, see for details:
+     * https://stackoverflow.com/questions/10394857/how-to-use-transactional-with-spring-data#comment13524479_10466591
+     */
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     Mono<Employee> findEmployeeById(String id);
 
     Flux<Employee> findAllEmployees();
